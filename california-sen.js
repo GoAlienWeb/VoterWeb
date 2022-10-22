5d710e8093e3419f57272598c795a4551e97f0eb
Alien Web Development <76794694+GoAlienWeb@users.noreply.github.com>
2022-10-22 10:39:40 -0500

    Add files via upload

---
+++ california-sen.js
@@ -0,0 +1,25 @@
+var raceTitle = "California Senate Elections";
+
+var race = ["Senate Seat"];
+             
+var candidate =
+[
+   ["Padilla, Alex", "Meuser, Mark", "Multiple Candidates"],
+  
+];
+
+var party =
+[
+   ["D", "R", "L"],
+         
+ ];
+ 
+ var votes = 
+ [
+   [3725544, 1028374, 2130147],
+    
+ ];
+
+
+
+
