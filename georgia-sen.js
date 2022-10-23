6d783c2aa652ec84b1fa7ba957591480d63f0684
Alien Web Development <76794694+GoAlienWeb@users.noreply.github.com>
2022-10-23 11:10:25 -0500

    Add files via upload

---
+++ georgia-sen.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var raceTitle = "Georgia Senate Elections";
+
+var race = ["Senate Seat"];
+             
+var candidate =
+[
+   ["Perdue, David", "Ossoff, Jon", "No Third Party"],
+  
+];
+
+var party =
+[
+   ["R", "D", "I"],
+         
+ ];
+ 
+ var votes = 
+ [
+   [2462617, 2374519, 0],
+    
+ ];
+
+
+
+
+
+
