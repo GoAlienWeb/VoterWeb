a1b826c5ec991f9319e51224bf262133c5b0913b
Alien Web Development <76794694+GoAlienWeb@users.noreply.github.com>
2022-10-23 14:16:56 -0500

    Add files via upload

---
+++ kansas-sen.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var raceTitle = "Kansas Senate Elections";
+
+var race = ["Senate Seat"];
+             
+var candidate =
+[
+   ["Marshall, Roger", "Bollier, Barbara", "No Third Party"],
+  
+];
+
+var party =
+[
+   ["R", "D", "I"],
+         
+ ];
+ 
+ var votes = 
+ [
+   [727962, 571530, 0],
+    
+ ];
+
+
+
+
+
+
