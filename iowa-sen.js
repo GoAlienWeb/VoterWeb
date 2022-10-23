d608a95573dd22d6bae645f1bdc601acc4ea90a8
Alien Web Development <76794694+GoAlienWeb@users.noreply.github.com>
2022-10-23 14:15:37 -0500

    Add files via upload

---
+++ iowa-sen.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var raceTitle = "Iowa Senate Elections";
+
+var race = ["Senate Seat"];
+             
+var candidate =
+[
+   ["Ernst, Joni", "Greenfield, Theresa", "No Third Party"],
+  
+];
+
+var party =
+[
+   ["R", "D", "I"],
+         
+ ];
+ 
+ var votes = 
+ [
+   [864997, 754859, 0],
+    
+ ];
+
+
+
+
+
+
