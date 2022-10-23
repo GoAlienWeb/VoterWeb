470dc1290e5a2f385fbe1f65d4aebca61a72030c
Alien Web Development <76794694+GoAlienWeb@users.noreply.github.com>
2022-10-23 11:04:22 -0500

    Add files via upload

---
+++ delaware-sen.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var raceTitle = "Delaware Senate Elections";
+
+var race = ["Senate Seat"];
+             
+var candidate =
+[
+   ["Coons, Chris", "Witzke, Lauren", "No Third Party"],
+  
+];
+
+var party =
+[
+   ["D", "R", "I"],
+         
+ ];
+ 
+ var votes = 
+ [
+   [291804, 186054, 0],
+    
+ ];
+
+
+
+
+
+
