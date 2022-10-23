22943d63871e038b0fe3e472f6f3aa7fb8d2cb22
Alien Web Development <76794694+GoAlienWeb@users.noreply.github.com>
2022-10-23 14:19:06 -0500

    Add files via upload

---
+++ kentucky-sen.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var raceTitle = "Kentucky Senate Elections";
+
+var race = ["Senate Seat"];
+             
+var candidate =
+[
+   ["McConnell, Mitch", "McGrath, Amy", "No Third Party"],
+  
+];
+
+var party =
+[
+   ["R", "D", "I"],
+         
+ ];
+ 
+ var votes = 
+ [
+   [1233315, 816257, 0],
+    
+ ];
+
+
+
+
+
+
