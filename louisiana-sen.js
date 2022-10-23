09cf1919e4f995a67b84a7831e7e299eb7bb8fac
Alien Web Development <76794694+GoAlienWeb@users.noreply.github.com>
2022-10-23 14:20:12 -0500

    Add files via upload

---
+++ louisiana-sen.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var raceTitle = "Louisiana Senate Elections";
+
+var race = ["Senate Seat"];
+             
+var candidate =
+[
+   ["Cassidy, Bill", "Perkins, Adrian", "Other Democrats"],
+  
+];
+
+var party =
+[
+   ["R", "D", "D"],
+         
+ ];
+ 
+ var votes = 
+ [
+   [1228908, 394049, 285524],
+    
+ ];
+
+
+
+
+
+
