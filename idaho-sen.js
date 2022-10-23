3e4ffab2de6c5eccf6636e6a2296d070a1fafd4c
Alien Web Development <76794694+GoAlienWeb@users.noreply.github.com>
2022-10-23 11:20:05 -0500

    Add files via upload

---
+++ idaho-sen.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var raceTitle = "Idaho Senate Elections";
+
+var race = ["Senate Seat"];
+             
+var candidate =
+[
+   ["Risch, Jim", "Jordan, Paulette", "No Third Party"],
+  
+];
+
+var party =
+[
+   ["R", "D", "I"],
+         
+ ];
+ 
+ var votes = 
+ [
+   [538446, 285864, 0],
+    
+ ];
+
+
+
+
+
+
