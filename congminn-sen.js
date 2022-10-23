cb1a01803a0f70a26a0fca142b3dc5cbd4010bc6
Alien Web Development <76794694+GoAlienWeb@users.noreply.github.com>
2022-10-23 14:27:16 -0500

    Add files via upload

---
+++ congminn-sen.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var raceTitle = "Minnesota Senate Elections";
+
+var race = ["Senate Seat"];
+             
+var candidate =
+[
+   ["Smith, Tina", "Lewis, Jason", "No Third Party"],
+  
+];
+
+var party =
+[
+   ["D", "R", "I"],
+         
+ ];
+ 
+ var votes = 
+ [
+   [1566522, 1398145, 0],
+    
+ ];
+
+
+
+
+
+
