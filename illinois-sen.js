be2c6b9a75c714b2ee449383606d0bcafb7da01f
Alien Web Development <76794694+GoAlienWeb@users.noreply.github.com>
2022-10-23 14:11:26 -0500

    Add files via upload

---
+++ illinois-sen.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var raceTitle = "Illinois Senate Elections";
+
+var race = ["Senate Seat"];
+             
+var candidate =
+[
+   ["Durbin, Richard", "Curran, Mark", "No Third Party"],
+  
+];
+
+var party =
+[
+   ["D", "R", "I"],
+         
+ ];
+ 
+ var votes = 
+ [
+   [3278930, 2319870, 0],
+    
+ ];
+
+
+
+
+
+
