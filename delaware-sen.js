b2e2679a5770a1f9039a9bb2af9baa09f644fc1d
Alien Web Development <76794694+GoAlienWeb@users.noreply.github.com>
2022-10-23 11:06:35 -0500

    Update delaware-sen.js

--- delaware-sen.js
+++ delaware-sen.js
@@ -6,19 +6,19 @@ var race = ["Senate Seat"];
              
 var candidate =
 [
-   ["Coons, Chris", "Witzke, Lauren", "No Third Party"],
+   ["Coons, Chris", "Witzke, Lauren", "Multiple Other Candidates"],
   
 ];
 
 var party =
 [
-   ["D", "R", "I"],
+   ["D", "R", "L"],
          
  ];
  
  var votes = 
  [
-   [291804, 186054, 0],
+   [291804, 186054, 13077],
     
  ];
 
