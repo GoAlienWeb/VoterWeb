1e47b8a8142fb57d0de2a9385ef19f6199c8cb43
Alien Web Development <76794694+GoAlienWeb@users.noreply.github.com>
2022-10-23 10:57:38 -0500

    Add files via upload

---
+++ colorado-sen.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var raceTitle = "Colorado Senate Elections";
+
+var race = ["Senate Seat"];
+             
+var candidate =
+[
+   ["Hickenlooper, John", "Gardner, Cory", "No Third Party"],
+  
+];
+
+var party =
+[
+   ["D", "R", "I"],
+         
+ ];
+ 
+ var votes = 
+ [
+   [1731114, 1429492, 0],
+    
+ ];
+
+
+
+
+
+
