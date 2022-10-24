7df3fafa282fad082b81718ff9f49eefc234df5b
Alien Web Development <76794694+GoAlienWeb@users.noreply.github.com>
2022-10-24 12:25:19 -0500

    Add files via upload

---
+++ alabama-gov.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var raceTitle = "Alabama Congressional Elections";
+
+var race = ["Governor"];
+             
+var candidate =
+[
+   ["No Election This Cycle"],
+   
+];
+
+var party =
+[
+   ["R", "D", "I"],
+   ["R", "D", "I"],
+   ["R", "D", "I"],
+   ["R", "D", "I"],
+   ["R", "D", "I"],
+   ["R", "D", "I"],
+   ["D", "R", "I"],               
+  
+ ];
+ 
+ var votes = 
+ [
+   [0, 0, 0],
+  
+ ];
+
+
+
+
+
+
