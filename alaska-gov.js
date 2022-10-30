3598b662cd476f9387470594315da05764133e35
Richard Olsen <76794694+GoAlienWeb@users.noreply.github.com>
2022-10-30 11:30:11 -0500

    Add files via upload

---
+++ alaska-gov.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var raceTitle = "Alaska Gubernatorial Election";
+
+var race = ["Governor"];
+             
+var candidate =
+[
+   ["No Election This Cycle"],
+   
+];
+
+var party =
+[
+   ["R", "D", "I"],           
+  
+ ];
+ 
+ var votes = 
+ [
+   [0, 0, 0],
+  
+ ];
+
+
+
+
+
+
